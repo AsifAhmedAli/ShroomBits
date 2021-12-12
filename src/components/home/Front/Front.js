2d207f6d30b394ad123d98de41966a1c5911d0fb
kazmi066 <awaisabbas47@gmail.com>
2021-12-12 21:04:19 +0500

    modal added

--- src/components/home/Front/Front.js
+++ src/components/home/Front/Front.js
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import front_shrooms from "../../../assets/front_shrooms.png";
 import './Front.css';
+import Modals from "./Modals";
 
 export default function Front() {
+    const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
+    const [counter, setCounter] = useState(1);
+
+    const changeModal = () => {
+        setModal(!modal);
+        setModal2(!modal2);
+    }
+
+    const counterIncrement = () => {
+        setCounter(counter + 1);
+    }
+
+    const counterDecrement = () => {
+        setCounter(counter - 1);
+    }
+
     return (
         <section id="front">
+            <Modals modal={modal}
+                setModal={setModal}
+                modal2={modal2}
+                setModal2={setModal2}
+                counterIncrement={counterIncrement}
+                counterDecrement={counterDecrement}
+                counter={counter}
+                changeModal={changeModal}
+            />
+
             <Container>
                 <div className="content">
                     <h1 className="top_title text-uppercase my-4">
                         <span>shroom</span>
                         <span>bits</span>
                     </h1>
-                    <img src={front_shrooms} alt="shrooms" />
+                    <img src={front_shrooms}
+                        alt="shrooms" />
                     <h2 className="cute_title text-capitalize"><span>cute cross chain</span> <span>nfts</span></h2>
                     <h3 className="sold">1482/1500 of Season 1 Shrooms are sold out!</h3>
                     <div className="front_btns">
-                        <Button>Mint gen0 shrooms</Button>
+                        <Button onClick={() => setModal(true)}>Mint gen0 shrooms</Button>
                         <Button>launch metaverse</Button>
                     </div>
                 </div>
---
+++ src/components/home/Front/Modals.js
@@ -0,0 +1,55 @@
+import { Button } from "react-bootstrap";
+
+export default function Modals({ modal, setModal, modal2, setModal2, counterIncrement, counterDecrement, counter, changeModal }) {
+    return (
+        <div>
+            {modal && <div className="first_modal">
+                <div className="close_modal">
+                    <i className="fas fa-times" onClick={() => setModal(false)}></i>
+                </div>
+                <div className="modal_content">
+                    <div className="modal_top text-center">
+                        <h5 className="mb-1">Price <b>0.06 ETH/WETH</b></h5>
+                        <h6>Shrooms minted: <b>1482</b></h6>
+                    </div>
+                    <div className="modal_bottom text-center">
+                        <h4>
+                            <b>How many Shrooms would <br /> you like to mint?</b>
+                        </h4>
+                        <div className="counter">
+                            <button className="calc" onClick={counterDecrement}>-</button>
+                            <div className="count_val">{counter}</div>
+                            <button className="calc" onClick={counterIncrement}>+</button>
+                        </div>
+                        <div className="modal_btns">
+                            <Button>Etherium</Button>&nbsp;&nbsp;&nbsp;
+                            <Button onClick={changeModal}>Matic (Polygon)</Button>
+                        </div>
+                        <div className="modal_cancel">
+                            <Button onClick={() => setModal(false)}>Cancel</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+
+            {modal2 && <div className="second_modal">
+                <div className="close_modal" onClick={() => setModal(false)}>
+                    <i className="fas fa-times" onClick={changeModal}></i>
+                </div>
+                <div className="modal_content">
+                    <div className="modal_top text-center">
+                        <h3>Step 1</h3>
+                    </div>
+                    <div className="modal_bottom text-center">
+                        <h3 className="my-4 fw-bold">Approve WETH</h3>
+                        <Button>Approve</Button>
+
+                        <div className="mt-2 modal_cancel">
+                            <Button onClick={changeModal}>Cancel</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+        </div>
+    )
+}
